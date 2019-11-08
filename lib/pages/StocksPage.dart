7abcdf0d912377ed3a05149667a77f50a7f2a951
Basil Zuberi <basilzuberi@hotmail.com>
2019-11-07 20:31:31 -0500

    added textfield

--- lib/pages/StocksPage.dart
+++ lib/pages/StocksPage.dart
@@ -5,8 +5,16 @@ class StocksPage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return new Center(
-      child: new TextField()
+    return new Scaffold(
+      body: new Container(
+        child: new Column(
+          children: <Widget>[
+            new TextField(
+              decoration: InputDecoration(hintText: "Enter Stock Symbol [Ex. AMZN]"),
+            )
+          ],
+        ),
+      ),
     );
 
   }
