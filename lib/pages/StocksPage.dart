c2885f0c0aa69a6492772c81259a1e3ae1d5e7b7
Basil Zuberi <basilzuberi@hotmail.com>
2020-02-19 17:03:17 -0500

    added current price section

--- lib/pages/StocksPage.dart
+++ lib/pages/StocksPage.dart
@@ -18,15 +18,75 @@ class _StocksPageState extends State<StocksPage> {
             new TextField(
               decoration: InputDecoration(hintText: "Enter Stock Symbol [Ex. AMZN]"),
             ),
-            Padding(
-              padding: EdgeInsets.all(15.0),
-              child: thisStock("MSFT"),
+            
+            Expanded(
+              child: StaggeredGridView.count(
+                crossAxisCount: 4,
+                crossAxisSpacing: 12.0,
+                mainAxisSpacing: 12.0,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: thisStock("MSFT"),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: currentPrice("PRICE"),
+                  ),
+                ],
+                staggeredTiles: [
+                  StaggeredTile.extent(4, 250),
+                  StaggeredTile.extent(2, 150)
+                ],
+              ),
             ),
           ],
         ),
       ),
     );
   }
+
+  Material currentPrice(String title){
+    return Material(
+      color: Color(0xff212F3D),
+      elevation: 14.0,
+      borderRadius: BorderRadius.circular(24.0),
+      shadowColor: Color(0x802196F3),
+      child: Center(
+        child:Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment:MainAxisAlignment.center,
+            children: <Widget>[
+              Column(
+                mainAxisAlignment:MainAxisAlignment.center,
+               children: <Widget>[
+
+                  Padding(
+                   padding: EdgeInsets.all(8.0),
+                      child:Text(title,style:TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.white,
+                      ),),
+                    ),
+
+                    Padding(
+                   padding: EdgeInsets.all(8.0),
+                      child:Text("250",style:TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.white,
+                      ),),
+                    ),
+
+               ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
   var data = [187.2300,185.3500,183.7100,184.7100,184.4400,188.7000,183.8900];
   Material thisStock(String title){
       return Material(
