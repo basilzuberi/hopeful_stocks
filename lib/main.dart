ffd5f45e757f947d654dc0ed20378cc326445de4
Basil Zuberi <basilzuberi@hotmail.com>
2020-02-27 17:57:54 -0500

    Added new dependancies for Alpha Vantage Package.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:csv/csv.dart';
+
 import 'HomePage.dart';
 
 main() {
--- lib/models/StocksModel.dart
+++ lib/models/StocksModel.dart
@@ -1,7 +1,7 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/foundation.dart';
+// import 'package:flutter/material.dart';
+// import 'package:http/http.dart' as http;
 ///This class is to implement JSON queries for a stock the user will search for.
 
 class Stock{
